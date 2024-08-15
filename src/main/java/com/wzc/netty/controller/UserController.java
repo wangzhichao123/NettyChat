6335c45ba20ac0432e9e9e6bd85d10dbdacc73ad
wangzhichao123 <1477099521@qq.com>
2024-08-15 21:52:39 +0800

    增加【添加/搜索】好友功能

--- src/main/java/com/wzc/netty/controller/UserController.java
+++ src/main/java/com/wzc/netty/controller/UserController.java
@@ -26,28 +26,28 @@ public class UserController {
 
     @PostMapping("/relationship")
     @ApiOperation(value = "获取用户好友列表")
-    public R<List<UserFriendsInfoVo>> getUserFriendList(@RequestParam("userId") @Valid @NotBlank String userId) {
+    public R<List<UserFriendsInfoVo>> getUserFriendList(@RequestParam("userId") @NotBlank String userId) {
         return R.ok(userService.getUserFriendListByUserId(userId));
     }
 
     @PostMapping("/search/relationship")
     @ApiOperation(value = "获取搜索用户信息")
-    public R<UserSearchInfoVo> getSearchUserInfo(@RequestParam("userId") @Valid @NotBlank String userId) {
+    public R<UserSearchInfoVo> getSearchUserInfo(@RequestParam("userId") @NotBlank String userId) {
         return R.ok(userService.getSearchUserInfoByUserId(userId));
     }
 
     @PostMapping("/add")
     @ApiOperation(value = "添加好友")
-    public R<Boolean> addUser(@RequestParam("userFromId") @Valid @NotBlank String userFromId,
-                              @RequestParam("userToId") @Valid @NotBlank String userToId) {
+    public R<Boolean> addUser(@RequestParam("userFromId") @NotBlank String userFromId,
+                              @RequestParam("userToId") @NotBlank String userToId) {
         return R.ok(userService.addUser(userFromId, userToId));
     }
 
     @PostMapping("/approve")
     @ApiOperation(value = "通过/拒绝好友申请信息")
-    public R<Boolean> approveUser(@RequestParam("userFromId") @Valid @NotBlank String userFromId,
-                                  @RequestParam("userToId") @Valid @NotBlank String userToId,
-                                  @RequestParam("flag") @Valid @NotBlank Boolean flag) {
+    public R<Boolean> approveUser(@RequestParam("userFromId") @NotBlank String userFromId,
+                                  @RequestParam("userToId") @NotBlank String userToId,
+                                  @RequestParam("flag") @NotBlank Boolean flag) {
         return R.ok(userService.approveOrRejectUser(userFromId, userToId, flag));
     }
 
